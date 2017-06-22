adeac63fa8c30d81949c81651f2915d0824c3471
Roman <rom.lviv.ua@gmail.com>
2017-06-22 15:54:47 +0300

    delete elemens

--- newtest/js/main.js
+++ newtest/js/main.js
@@ -1,11 +1,25 @@
+var a = 1;
 function addElement() {
 	var fname = document.getElementById("fname");
 	var lname = document.getElementById("lname");
 	var container = document.getElementById("container");
 	var element = document.createElement("div");
-	element.innerHTML = lname.value + " " + fname.value;
+	element.id = "reload"+a;
+
+	var deleteEl = document.createElement("button");
+	deleteEl.id = "reload"+a;
+	deleteEl.setAttribute('onclick', 'dell(this.id);')
+	var textBtn = document.createTextNode("Delete");
+	element.innerHTML = lname.value + " " + fname.value + " ";
+	deleteEl.appendChild(textBtn);
+	element.appendChild(deleteEl);
 	container.appendChild(element);
 
 	fname.value = "";
 	lname.value = "";
+	a++;
+}
+function dell(deleteElId){
+	var element = document.getElementById(deleteElId);
+	element.remove();
 }
