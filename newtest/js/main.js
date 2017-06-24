a9d251210baec3c9fabf0f29724d08506d2f1933
Roman <rom.lviv.ua@gmail.com>
2017-06-24 13:00:37 +0300

    Change of functions

--- newtest/js/main.js
+++ newtest/js/main.js
@@ -2,40 +2,14 @@ var a = 1;
 function addElement() {
 
 	var container = document.getElementById("container");
-	var element = document.createElement("div");
 	var tempId = "reload"+a;
-
-	var fnameSpan = document.createElement("span");
-	fnameSpan.id = tempId+"fname";
-	
-	var lnameSpan = document.createElement("span");
-	lnameSpan.id = tempId+"lname";
-
-	var deleteEl = document.createElement("button");
-	deleteEl.id = tempId;
-	deleteEl.setAttribute('onclick', 'deleteElement(this.id);');
-	var textBtnDell = document.createTextNode("Delete");
-
-	var updateEl = document.createElement("button");
-	updateEl.name = tempId;
-	updateEl.setAttribute('onclick', 'update(this.name);');
-	var textBtnUpdate = document.createTextNode("Update");
-
-	deleteEl.appendChild(textBtnDell);
-	updateEl.appendChild(textBtnUpdate);
-	
-	element.appendChild(lnameSpan);
-	element.appendChild(fnameSpan);
-
-	element.appendChild(deleteEl);
-	element.appendChild(updateEl);
+	element = renderItem();
 	container.appendChild(element);
-
-	renderItem(tempId);
+	fillSpan(tempId); 	
 	a++;
-
 }
 function deleteElement(deleteElId){
+	// console.log(document.getElementById(deleteElId));
 	var element = document.getElementById(deleteElId);
 	element.remove();
 }
@@ -52,13 +26,51 @@ function completionUpdate(){
 	var idDiv = document.getElementById("clickBtn").dataset.id;
 	renderItem(idDiv);
 }
- function renderItem(idDiv){
- 	var fname = document.getElementById("fname");
-	var lname = document.getElementById("lname");
- 	document.getElementById(idDiv+"fname").innerHTML = fname.value + " ";
-	document.getElementById(idDiv+"lname").innerHTML = lname.value + " ";
-	fname.value = "";
-	lname.value = "";
+function renderItem(idDiv){
+	if (idDiv) {
+		fillSpan(idDiv);
+	} else {
+
+		var container = document.getElementById("container");
+		var element = document.createElement("div");
+		var tempId = "reload"+a;
+
+		var fnameSpan = document.createElement("span");
+		fnameSpan.id = tempId+"fname";
+		
+		var lnameSpan = document.createElement("span");
+		lnameSpan.id = tempId+"lname";
+
+		var deleteEl = document.createElement("button");
+		deleteEl.id = tempId;
+		deleteEl.setAttribute('onclick', 'deleteElement(this.id);');
+		var textBtnDell = document.createTextNode("Delete");
+
+		var updateEl = document.createElement("button");
+		updateEl.name = tempId;
+		updateEl.setAttribute('onclick', 'update(this.name);');
+		var textBtnUpdate = document.createTextNode("Update");
+
+		deleteEl.appendChild(textBtnDell);
+		updateEl.appendChild(textBtnUpdate);
+		
+		element.appendChild(lnameSpan);
+		element.appendChild(fnameSpan);
+
+		element.appendChild(deleteEl);
+		element.appendChild(updateEl);
+		
+		return element
+	}
  }
 
 
+function fillSpan(idDiv){
+console.log(idDiv);
+	var fname = document.getElementById("fname");
+	var lname = document.getElementById("lname");
+	document.getElementById(idDiv+"fname").innerHTML = fname.value + " ";
+	document.getElementById(idDiv+"lname").innerHTML = lname.value + " ";
+	fname.value = "";
+	lname.value = "";
+}
