7ba13e35edda57e3ba7015bc9b40db0246fe6262
Roman <rom.lviv.ua@gmail.com>
2017-06-29 12:11:51 +0300

    fixed problems

--- newtest/js/main.js
+++ newtest/js/main.js
@@ -1,62 +1,72 @@
 var a = 1;
-function addElement() {
-
-	var container = document.getElementById("container");
-	var tempId = "reload"+a;
-
-	var element = document.createElement("div");
-	element.id = tempId;
-	
-	var fnameSpan = document.createElement("span");
-	var lnameSpan = document.createElement("span");
-
+function createDelBtn(){
 	var deleteEl = document.createElement("button");
 	deleteEl.setAttribute("onclick", "deleteElement(this.parentNode.id)");
 	var textBtnDell = document.createTextNode("Delete");
+	deleteEl.appendChild(textBtnDell);
+	return deleteEl
+}
 
+function createUpdBtn(){
 	var updateEl = document.createElement("button");
 	updateEl.setAttribute("onclick", "update(this.parentNode.id)");
 	var textBtnUpdate = document.createTextNode("Update");
-
-	deleteEl.appendChild(textBtnDell);
 	updateEl.appendChild(textBtnUpdate);
-	
-	element.appendChild(lnameSpan);
-	element.appendChild(fnameSpan);
+	return updateEl
+}
 
-	element.appendChild(deleteEl);
-	element.appendChild(updateEl);
+function createNameSpan(text){
+	var nameSpan = document.createElement("span");
+	nameSpan.innerHTML = text;
+	return nameSpan
+}
 
-	container.appendChild(element);
-	
+function createDiv(id,fname,lname){
+	var element = document.createElement("div");
+	element.id = id;
+	element.appendChild(createNameSpan(fname));
+	element.appendChild(createNameSpan(lname));
+	element.appendChild(createDelBtn());
+	element.appendChild(createUpdBtn());
+	return element
+}
+
+function addElement() {
+	var container = document.getElementById("container");
+	var clickBtn = document.getElementById("clickBtn");
+	var idDiv = clickBtn.dataset.id;
 	var fname = document.getElementById("fname");
 	var lname = document.getElementById("lname");
-	fnameSpan.innerHTML = fname.value + " ";
-	lnameSpan.innerHTML = lname.value + " ";
-	fname.value = "";
-	lname.value = "";	
-	a++;
-	return tempId;
+
+	if(!idDiv){
+		var tempId = "reload"+a;
+		var element = createDiv(tempId,fname.value,lname.value);
+		container.appendChild(element);
+		a++;
+	} else {
+		container.replaceChild(createDiv(idDiv,fname.value,lname.value), document.getElementById(idDiv));
+	}
+		clearForm();
+	
+}
+
+function clearForm(){
+	document.getElementById("fname").value = "";
+	document.getElementById("lname").value = "";
+	document.getElementById("clickBtn").value = "Add";
+	clickBtn.dataset.id = "";
+}
+
+function update(updateElId){
+	var clickBtn = document.getElementById("clickBtn");
+	clickBtn.value = "Ok";
+	clickBtn.dataset.id = updateElId;
+	document.getElementById(updateElId).style.backgroundColor = "red";
+	document.getElementById("fname").value = document.getElementById(updateElId).childNodes[0].textContent;
+	document.getElementById("lname").value = document.getElementById(updateElId).childNodes[1].textContent;
 }
 function deleteElement(deleteElId){
 	var element = document.getElementById(deleteElId);
 	element.remove();
 }
-function update(updateElId){
-	document.getElementById("clickBtn").value = "Ok";
-	document.getElementById("clickBtn").onclick = completionUpdate;
-	document.getElementById("clickBtn").dataset.id = updateElId;
-	document.getElementById("fname").value = document.getElementById(updateElId).childNodes[1].textContent;
-	document.getElementById("lname").value = document.getElementById(updateElId).childNodes[0].textContent;
-}
-function completionUpdate(){
-	document.getElementById("clickBtn").value = "Add";
-	document.getElementById("clickBtn").onclick = renderItem;
-	var idDiv = document.getElementById("clickBtn").dataset.id;
-	renderItem(idDiv);
-}
-function renderItem(idDiv){
-	var temp = addElement();
-	container.replaceChild(document.getElementById(temp), document.getElementById(idDiv));
 
-}
